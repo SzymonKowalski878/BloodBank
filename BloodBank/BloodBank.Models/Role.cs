4fa43aa6acdfe186c18aa1b770fb1719ef59483c
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-11 14:30:50 +0200

    Created Role model

---
+++ BloodBank/BloodBank.Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Models
+{
+    public class Role:BaseModel
+    {
+        public int Id { get; set; }
+        public bool IsWorker { get; set; }
+        public bool IsBloodDonator { get; set; }
+        public string UserEmail { get; set; }
+        public User User { get; set; }
+    }
+}
