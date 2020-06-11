223c8f9b362147bb3d3e3214f5933cb2cfe73da3
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-11 16:11:58 +0200

    Added filed to BloodDonator and BloodStorage, fixed error in User

--- BloodBank/BloodBank.Models/BloodDonator.cs
+++ BloodBank/BloodBank.Models/BloodDonator.cs
@@ -12,6 +12,7 @@ namespace BloodBank.Models
         public string HomeAdress { get; set; }
         public string PhoneNumber { get; set; }
         public int AmmountOfBloodDonated { get; set; }
+        public int Pesel { get; set; }
         public virtual string BloodGroupName { get; set; }
         public virtual BloodType BloodType { get; set; }
         public virtual string UserEmail { get; set; }
--- BloodBank/BloodBank.Models/BloodStorage.cs
+++ BloodBank/BloodBank.Models/BloodStorage.cs
@@ -6,6 +6,7 @@ namespace BloodBank.Models
 {
     public class BloodStorage:BaseModel
     {
+        public int Id { get; set; }
         public int StoredAmmount { get; set; }
         public virtual string BloodGroupName { get; set; }
         public virtual BloodType BloodType { get; set; }
