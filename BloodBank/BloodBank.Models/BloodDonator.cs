c24e63aa383f5c1b37636cf99f4adf531527459d
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-11 14:29:53 +0200

    Created BloodDonator model

---
+++ BloodBank/BloodBank.Models/BloodDonator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Models
+{
+    public class BloodDonator:BaseModel
+    {
+        public DateTime BirthDate { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string HomeAdress { get; set; }
+        public string PhoneNumber { get; set; }
+        public int AmmountOfBloodDonated { get; set; }
+        public virtual string BloodGroupName { get; set; }
+        public virtual BloodType BloodType { get; set; }
+        public virtual string UserEmail { get; set; }
+        public virtual User User { get; set; }
+    }
+}
