561915463d7cca51ea53632594d8b7ca2848e4a8
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-13 00:15:27 +0200

    Created UserRepository and implemented IUserRepository

---
+++ BloodBank/BloodBank.DataAccess/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using BloodBank.Logics.Repositories;
+using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodBank.DataAccess
+{
+    public class UserRepository:Repository<User>,IUserRepository
+    {
+        public UserRepository(Lazy<DataContext> dataContext)
+            :base(dataContext)
+        {
+            
+        }
+
+        public IQueryable<User> GetUsersByLogin(string email)
+        {
+            return DataContext.Users.Where(m => m.Email == email);
+        }
+
+        public string GetUserPassword(string email)
+        {
+            var list = GetUsersByLogin(email);
+            var result = list.FirstOrDefault();
+
+            return result?.Password;
+        }
+    }
+}
