24f36c2ed1f366c3018522a712f443b9154e936b
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-12 23:51:54 +0200

    Created interface for user repository

---
+++ BloodBank/BloodBank.Logics/Repositories/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Repositories
+{
+    public interface IUserRepository:IRepository<User>
+    {
+    }
+}
