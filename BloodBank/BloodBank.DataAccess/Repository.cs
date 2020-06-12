b36f7606adcc1b157ccadeb359bb5ad049eab168
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-12 23:50:27 +0200

    Created generic repository, implemented IRepository

---
+++ BloodBank/BloodBank.DataAccess/Repository.cs
@@ -0,0 +1,34 @@
+﻿using BloodBank.Logics.Repositories;
+using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.DataAccess
+{
+    public class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        private readonly Lazy<DataContext> _dataContext;
+        protected DataContext DataContext => _dataContext.Value;
+
+        public Repository(Lazy<DataContext> dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public virtual void Delete(T model)
+        {
+            DataContext.Remove(model);
+        }
+
+        public virtual void SaveChanges()
+        {
+            DataContext.SaveChanges();
+        }
+
+        public virtual IEnumerable<T> GetAll()
+        {
+            return DataContext.Set<T>();
+        }
+    }
+}
