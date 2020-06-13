7fa68ea915f89e89ecb4246e20f7bc04a39f27bb
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-13 22:27:55 +0200

    Created IBloodTypeLogic

---
+++ BloodBank/BloodBank.Logics/Interfaces/IBloodTypeLogic.cs
@@ -0,0 +1,14 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Interfaces
+{
+    public interface IBloodTypeLogic:ILogic
+    {
+        Result<BloodType> Add(BloodType model);
+        Result<BloodType> Remove(BloodType model);
+        Result<BloodType> GetByName(string name);
+    }
+}
