e0fc316316b9f1de1d3a001cb97970956d50928d
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-14 01:30:55 +0200

    Created IStorageLogic

---
+++ BloodBank/BloodBank.Logics/Interfaces/IStorageLogic.cs
@@ -0,0 +1,15 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Interfaces
+{
+    public interface IStorageLogic:ILogic
+    {
+        Result<BloodStorage> Add(BloodStorage model);
+        Result<BloodStorage> Remove(BloodStorage model);
+        Result<BloodStorage> GetByBloodTypeName(string name);
+        Result<BloodStorage> AddBloodToStorage(string name, int ammount);
+    }
+}
