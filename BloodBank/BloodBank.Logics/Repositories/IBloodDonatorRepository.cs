f245f762be7e65872f0be18f2c30d6f1ef5bac9b
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-13 14:08:12 +0200

    Created interface for BloodDonatorRepository

---
+++ BloodBank/BloodBank.Logics/Repositories/IBloodDonatorRepository.cs
@@ -0,0 +1,12 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Repositories
+{
+    public interface IBloodDonatorRepository:IRepository<BloodDonator>
+    {
+        BloodDonator GetByPesel(int pesel);
+    }
+}
