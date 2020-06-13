5f02675978d18bbb39ca2c37b9618652d90a709f
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-14 01:34:18 +0200

    Created IUserLogic

---
+++ BloodBank/BloodBank.Logics/Interfaces/IUserLogic.cs
@@ -0,0 +1,15 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Interfaces
+{
+    public interface IUserLogic:ILogic
+    {
+        Result<User> AddBloodDonator(User user);
+        Result<User> AddWorker(User user);
+        Result<User> Remove(User model);
+        Result<User> GetByEmail(string email);
+    }
+}
