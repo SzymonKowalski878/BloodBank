095b921d3214b1a31f6c6a8546aa0cc6bd3c24bc
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-13 13:16:24 +0200

    Created BloodStorageRepository and implemented IBloodStorageRepository

---
+++ BloodBank/BloodBank.DataAccess/BloodStorageRepository.cs
@@ -0,0 +1,22 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BloodBank.DataAccess
+{
+    public class BloodStorageRepository:Repository<BloodStorage>
+    {
+        public BloodStorageRepository(Lazy<DataContext> dataContext)
+            : base(dataContext)
+        {
+
+        }
+
+        public BloodStorage GetByBloodTypeName(string name)
+        {
+            return DataContext.Storage.FirstOrDefault(m => m.BloodGroupName == name);
+        }
+    }
+}
