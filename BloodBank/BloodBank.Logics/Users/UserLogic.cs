75aaa565ee54fd6522765ee34f38df74e67f74d6
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-17 01:28:32 +0200

    Updated AddWorker and AddBloodDonator methods to check conditions

--- BloodBank/BloodBank.Logics/Users/UserLogic.cs
+++ BloodBank/BloodBank.Logics/Users/UserLogic.cs
@@ -11,10 +11,13 @@ namespace BloodBank.Logics.Users
     {
         private readonly Lazy<IUserRepository> _userRepository;
         protected IUserRepository UserRepository => _userRepository.Value;
-
-        public UserLogic(Lazy<IUserRepository> userRepository)
+        private readonly Lazy<IBloodTypeRepository> _bloodTypeRepository;
+        protected IBloodTypeRepository BloodTypeRepository => _bloodTypeRepository.Value;
+        public UserLogic(Lazy<IUserRepository> userRepository,
+            Lazy<IBloodTypeRepository>bloodTypeRepository)
         {
             _userRepository = userRepository;
+            _bloodTypeRepository = bloodTypeRepository;
         }
 
         public Result<User> AddWorker(User model)
@@ -23,12 +26,31 @@ namespace BloodBank.Logics.Users
             {
                 throw new ArgumentNullException(nameof(model));
             }
-            model.Role = new Role()
+            var userInDB = UserRepository.GetUserByLogin(model.Email);
+            //check if user already exists in database
+            //and if he is worker
+            //if so return error
+            if (userInDB!= null && userInDB.Role.IsWorker)
             {
-                IsWorker = true,
-                IsBloodDonator = false
-            };
-            UserRepository.Add(model);
+                return Result.Error<User>("User already exists and is worker");
+            }
+            //check if user already exists in database
+            //and if he isnt worker
+            //if so make him worker
+            else if (userInDB != null && !userInDB.Role.IsWorker)
+            {
+                userInDB.Role.IsWorker = true;
+            }
+            else
+            //else just add user
+            {
+                model.Role = new Role()
+                {
+                    IsWorker = true,
+                    IsBloodDonator = false
+                };
+                UserRepository.Add(model);
+            }
             UserRepository.SaveChanges();
 
             return Result.Ok(model);
@@ -40,14 +62,38 @@ namespace BloodBank.Logics.Users
             {
                 throw new ArgumentNullException(nameof(model));
             }
-            model.Role = new Role()
+            //check if the bloodtype passed to the function exists in db
+            if (!BloodTypeRepository.CheckIfExists(model.BloodDonator.BloodGroupName))
             {
-                IsWorker = false,
-                IsBloodDonator = true
-            };
-            UserRepository.Add(model);
-            UserRepository.SaveChanges();
+                return Result.Error<User>("Blood type passed to the function doesen't exist in database");
+            }
 
+            var userInDb = UserRepository.GetUserByLogin(model.Email);
+            //if user already exists and is blood donator return error
+            if (userInDb != null && userInDb.Role.IsBloodDonator)
+            {
+                return Result.Error<User>("User already exists and is blood donator");
+            }
+            //if user already exists but isnt blood donator
+            //make him a blooddonator and assign BloodDonator from model passed into the function
+            //to BloodDonator in user that already exist
+            else if (userInDb != null && !userInDb.Role.IsBloodDonator)
+            {
+                userInDb.BloodDonator = model.BloodDonator;
+                userInDb.Role.IsBloodDonator = true;
+            }
+            //if user doesent already exist, add new user that is blood donator but isnt worker
+            else if (userInDb == null)
+            {
+                model.Role = new Role()
+                {
+                    IsWorker = false,
+                    IsBloodDonator = true
+                };
+                UserRepository.Add(model);
+            }
+
+            UserRepository.SaveChanges();
             return Result.Ok(model);
         }
 
@@ -71,5 +117,6 @@ namespace BloodBank.Logics.Users
             }
             return Result.Ok(result);
         }
+
     }
 }
