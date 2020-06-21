5bdf152df1e31dd165fb194d0128e138a4bb914d
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-06-22 00:54:59 +0200

    Registered validators for dependeny injection with validatormodule

---
+++ BloodBank/BloodBank.Logics/Interfaces/IMyValidator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Interfaces
+{
+    //interface used for registratrion of validators
+    public interface IMyValidator
+    {
+    }
+}
---
+++ BloodBank/BloodBank.Web/Autofac/Modules/ValidatorModule.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using BloodBank.Logics.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BloodBank.Web.Autofac.Modules
+{
+    public class ValidatorModule:Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(IMyValidator).Assembly)
+                .Where(t => typeof(IMyValidator).IsAssignableFrom(t))
+                .AsImplementedInterfaces();
+        }
+    }
+}
