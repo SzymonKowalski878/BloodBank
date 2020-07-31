c47fa72b54711b495f8a9ce89d2bb9df477be777
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-08-01 00:58:31 +0200

    Created dataholder that contains login and password used for logging in

---
+++ BloodBank/BloodBank.Logics/Users/DataHolders/UserLoginAndPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Users.DataHolders
+{
+    public class UserLoginAndPassword
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
