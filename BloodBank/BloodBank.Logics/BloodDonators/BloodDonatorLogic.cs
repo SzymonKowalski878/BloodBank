979734c4d9f42df2f8e896872a3e0013746baa86
Szymon Kowalski <151517@student.uwm.edu.pl>
2020-08-31 00:30:27 +0200

    Created BloodDonatorLogic with method to update the amount of blood donated by a user

---
+++ BloodBank/BloodBank.Logics/BloodDonators/BloodDonatorLogic.cs
@@ -0,0 +1,41 @@
+﻿using BloodBank.Logics.Interfaces;
+using BloodBank.Logics.Repositories;
+using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.BloodDonators
+{
+    public class BloodDonatorLogic:IBloodDonatorLogic
+    {
+        private readonly Lazy<IBloodDonatorRepository> _bloodDonatorRepository;
+        protected IBloodDonatorRepository BloodDonatorRepository => _bloodDonatorRepository.Value;
+
+        public BloodDonatorLogic(Lazy<IBloodDonatorRepository> bloodDonatorRepository)
+        {
+            _bloodDonatorRepository = bloodDonatorRepository;
+        }
+
+        public Result<BloodDonator> UpdateDonatedBloodByUser(string pesel, int ammount)
+        {
+            if (string.IsNullOrEmpty(pesel) || ammount == 0)
+            {
+                return Result.Error<BloodDonator>("Pesel was null or ammount equaled 0");
+            }
+
+            var donatorToUpdate = BloodDonatorRepository.GetByPesel(pesel);
+
+            if (donatorToUpdate == null)
+            {
+                return Result.Error<BloodDonator>("Couldn't find blood donator with that pesel");
+            }
+
+            donatorToUpdate.AmmountOfBloodDonated += ammount;
+
+            BloodDonatorRepository.SaveChanges();
+
+            return Result.Ok(donatorToUpdate);
+        }
+    }
+}
---
+++ BloodBank/BloodBank.Logics/Interfaces/IBloodDonatorLogic.cs
@@ -0,0 +1,12 @@
+﻿using BloodBank.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BloodBank.Logics.Interfaces
+{
+    public interface IBloodDonatorLogic:ILogic
+    {
+        Result<BloodDonator> UpdateDonatedBloodByUser(string pesel, int ammount);
+    }
+}
